235b3ff464e9e3e55af3e4ebef2cf634b154a3fa
sharmin-akter23 <sharminakter18138@gmail.com>
2025-09-28 12:57:09 +0600

    hhfdiu

--- Salary_with_Bonus.c
+++ Salary_with_Bonus.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-int main()
-{
-int A,B;
-float C;
-
-scanf("%d\n",&A);
-scanf("%d\n",&B);
-scanf("%f\n",&C);
-
-float salary = B * C;
-printf("NUMBER = %d\n",A);
-printf("SALARY = U$ %.2f\n",salary);
-
- return 0;
-}
+5g65g5
